14b25b2c28813f54928036422fa8ee4fb0f32ce5
VVarHawk <67043126+VVarHawk@users.noreply.github.com>
2022-05-22 13:23:45 +0300

    Update test-account.cpp

--- lab_5/test/test-account.cpp
+++ lab_5/test/test-account.cpp
@@ -9,7 +9,7 @@ public:
     MockAccount(int id, int balance):Account(id, balance){}
     
     MOCK_METHOD(int, GetBalance, (), (const, override));
-    MOCK_METHOD(void, ChangeBalance, (int, override));
+    MOCK_METHOD(void, ChangeBalance, (int), (override));
     MOCK_METHOD(void, Lock, (), (override));
     MOCK_METHOD(void, Unlock, (), (override));
 };

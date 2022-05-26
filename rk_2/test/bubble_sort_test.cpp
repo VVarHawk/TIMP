80adc3a0ad870a5f7ad5a3d8170e689eb7dc2391
VVarHawk <67043126+VVarHawk@users.noreply.github.com>
2022-05-26 15:12:59 +0300

    Update bubble_sort_test.cpp

--- rk_2/test/bubble_sort_test.cpp
+++ rk_2/test/bubble_sort_test.cpp
@@ -1,28 +1,8 @@
-#include <iostream>
-#include <functional>
-#include <vector>
-#include <algorithm>
+#include "bubble_sort.h"
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
-void bubble_sort(std::vector<double> &arr)
-{
-    size_t arr_size = arr.size();
-
-    for(int i = 0; i < arr_size; ++i)
-    {
-        for(int j = i; j > 0; --j)
-        {
-            if(arr[j - 1] > arr[j])
-            {
-                double x = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = x;
-            }
-        }
-    }
-}
 
 TEST(sort_test, bubble_sort_test)
 {

e328668c0e3648dc9732afaabaf02deb8917164a
VVarHawk <67043126+VVarHawk@users.noreply.github.com>
2022-05-26 15:12:13 +0300

    Update bubble_sort.cpp

--- rk_2/src/bubble_sort.cpp
+++ rk_2/src/bubble_sort.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <algorithm>
 
+void bubble_sort(std::vector<double> &arr)
+{
+    size_t arr_size = arr.size();
+
+    for(int i = 0; i < arr_size; ++i)
+    {
+        for(int j = i; j > 0; --j)
+        {
+            if(arr[j - 1] > arr[j])
+            {
+                double x = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = x;
+            }
+        }
+    }
+}
